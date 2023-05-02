69cec7daf5ea7e0c6b64ded49f01fd3bec0f4ca8
BrokenHypocrite <brokenhypocrite@gmail.com>
2023-05-01 21:10:12 -0700

    Updated LobbyInfoDataParser for F1 23

--- src/parsers/packets/LobbyInfoDataParser.ts
+++ src/parsers/packets/LobbyInfoDataParser.ts
@@ -3,15 +3,14 @@ import {F1Parser} from '../F1Parser';
 export class LobbyInfoDataParser extends F1Parser {
   constructor(packetFormat: number) {
     super();
-    this.uint8('m_aiControlled')
-        .uint8('m_teamId')
-        .uint8('m_nationality')
-        .string('m_name', {length: 48, stripNull: true});
+    this.uint8('m_aiControlled').uint8('m_teamId').uint8('m_nationality');
 
-    if (packetFormat === 2021 || packetFormat === 2022) {
-      this.uint8('m_carNumber');
+    if (packetFormat === 2023) {
+      this.uint8('m_platform');
     }
 
-    this.uint8('m_readyStatus');
+    this.string('m_name', {length: 48, stripNull: true})
+      .uint8('m_carNumber')
+      .uint8('m_readyStatus');
   }
 }
