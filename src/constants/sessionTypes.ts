a9d9c908d75ddfb9e49811bc7ace6ac472fe606b
Mark Bryan <marklawbryan@gmail.com>
2024-05-27 10:46:33 -0700

    added year check for sessionTypes & teams. added AI_DRIVER_NAMES constant

--- src/constants/sessionTypes.ts
+++ src/constants/sessionTypes.ts
@@ -1,23 +1,42 @@
 import {SessionTypes} from './types';
 
-export const SESSION_TYPES: {[index: number]: SessionTypes} = {
-  0: {short: 'UNK', long: 'Unknown'},
-  1: {short: 'FP1', long: 'Free Practice 1'},
-  2: {short: 'FP2', long: 'Free Practice 2'},
-  3: {short: 'FP3', long: 'Free Practice 3'},
-  4: {short: 'ShortFP', long: 'Short Free Practice'},
-  5: {short: 'Q1', long: 'Qualifying 1'},
-  6: {short: 'Q2', long: 'Qualifying 2'},
-  7: {short: 'Q3', long: 'Qualifying 3'},
-  8: {short: 'ShortQ', long: 'Short Qualifying'},
-  9: {short: 'OneShotQ', long: 'One Shot Qualifying'},
-  10: {short: 'R', long: 'Race'}, // Sprint Shootout 1 in F1 24
-  11: {short: 'R2', long: 'Race 2'}, // Sprint Shootout 2 in F1 24
-  12: {short: 'R3', long: 'Race 3'}, // Sprint Shootout 3 in F1 24
-  13: {short: 'TT', long: 'Time Trial' }, // Short Sprint Shootout in F1 24
-  14: {short: 'OneShotS', long: 'One-Shot Sprint Shootout'}, // F1 24
-  15: {short: 'R', long: 'Race'}, // F1 24
-  16: {short: 'R2', long: 'Race 2'}, // F1 24
-  17: {short: 'R3', long: 'Race 3'}, // F1 24
-  18: {short: 'TT', long: 'Time Trial'}, // F1 24
-};
+export const SESSION_TYPES: {[year: number]: {[index: number]: SessionTypes}} =
+  {
+    2023: {
+      0: {short: 'UNK', long: 'Unknown'},
+      1: {short: 'FP1', long: 'Free Practice 1'},
+      2: {short: 'FP2', long: 'Free Practice 2'},
+      3: {short: 'FP3', long: 'Free Practice 3'},
+      4: {short: 'ShortFP', long: 'Short Free Practice'},
+      5: {short: 'Q1', long: 'Qualifying 1'},
+      6: {short: 'Q2', long: 'Qualifying 2'},
+      7: {short: 'Q3', long: 'Qualifying 3'},
+      8: {short: 'ShortQ', long: 'Short Qualifying'},
+      9: {short: 'OneShotQ', long: 'One Shot Qualifying'},
+      10: {short: 'R', long: 'Race'},
+      11: {short: 'R2', long: 'Race 2'},
+      12: {short: 'R3', long: 'Race 3'},
+      13: {short: 'TT', long: 'Time Trial'},
+    },
+    2024: {
+      0: {short: 'UNK', long: 'Unknown'},
+      1: {short: 'FP1', long: 'Free Practice 1'},
+      2: {short: 'FP2', long: 'Free Practice 2'},
+      3: {short: 'FP3', long: 'Free Practice 3'},
+      4: {short: 'ShortFP', long: 'Short Free Practice'},
+      5: {short: 'Q1', long: 'Qualifying 1'},
+      6: {short: 'Q2', long: 'Qualifying 2'},
+      7: {short: 'Q3', long: 'Qualifying 3'},
+      8: {short: 'ShortQ', long: 'Short Qualifying'},
+      9: {short: 'OneShotQ', long: 'One Shot Qualifying'},
+      10: {short: 'S1', long: 'Sprint Shootout 1'},
+      11: {short: 'S2', long: 'Sprint Shootout 2'},
+      12: {short: 'S3', long: 'Sprint Shootout 3'},
+      13: {short: 'ShortS', long: 'Short Sprint Shootout'},
+      14: {short: 'OneShotS', long: 'One-Shot Sprint Shootout'},
+      15: {short: 'R', long: 'Race'},
+      16: {short: 'R2', long: 'Race 2'},
+      17: {short: 'R3', long: 'Race 3'},
+      18: {short: 'TT', long: 'Time Trial'},
+    },
+  };
